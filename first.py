7adf790b77bcaf78f49fe763396c5321e1498272
Emanuele Bachi <emanuele.bachi@studenti.polito.it>
2017-02-11 11:37:02 +0100

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+euFloor = raw_input("European floor?")
+
+if int(euFloor) <= 0
+	usFloor = euFloor+1
+	print usFloor
+
+if int(euFloor) = 0
+	usFloor = "B"
+	print usFloor;
+	
+if !is_integer(euFloor)
+	print "Europeans use only numbers"
